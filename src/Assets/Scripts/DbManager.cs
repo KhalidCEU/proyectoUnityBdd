af2e90e0e48b1e982af00a88186d4b8069a6f8b3
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-05-03 22:22:34 +0200

    feat: add database connection and table creation

---
+++ src/Assets/Scripts/DbManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DBManager : MonoBehaviour
+{
+    private string dbName = "AdminApp.sqlite";
+
+    void Start()
+	{
+        string dbPath = Path.Combine(Application.persistentDataPath, dbName);
+        Debug.Log("DB path: " + dbPath);
+        string connString = "URI=file:" + dbPath;
+
+        var connection = new SqliteConnection(connString);
+
+        connection.Open();
+
+        Seeder.CreateTables(connection);
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+	}
+
+}
+
